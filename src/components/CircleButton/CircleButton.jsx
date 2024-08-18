503cbd32eda43445e17d40499da70530b871cb7b
sodmod <66504959+sodmod@users.noreply.github.com>
2024-08-18 12:40:44 +0100

    update

--- src/components/CircleButton/CircleButton.jsx
+++ src/components/CircleButton/CircleButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import 'circlebutton.css';
+import './circlebutton.css';
 
 const CircleButton = ({ onClick,icon }) => {
   return (
